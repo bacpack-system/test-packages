a62b063555ad103c5db1c0c3efaea65e4f069ec4
Mikuláš Šťastný <mikulas.stastny@bringauto.com>
2025-11-26 12:43:28 +0100

    Add apps

---
+++ test_app_1/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "app1" << std::endl;
+    return 0;
+}
---
+++ test_app_2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <pack5.hpp>
+
+int main() {
+    std::cout << "app1 " + pack5_str() << std::endl;
+    return 0;
+}
